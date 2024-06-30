a35a139ad3603715171c11a0268b85f5b7b28915
Rogue <rogue87.dev@gmail.com>
2024-06-30 18:01:24 +0200

    update script

--- setup.sh
+++ setup.sh
@@ -1,72 +1,54 @@
 #!/bin/bash
 
-## add repos
-
-# add lazygit repo
-sudo dnf copr enable atim/lazygit -y
-
-# add charm repo
-echo '[charm]
-name=Charm
-baseurl=https://repo.charm.sh/yum/
-enabled=1
-gpgcheck=1
-gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
-sudo yum install glow
-
 ## packages list
 packages=(
-   neovim
-   kitty
-   fastfetch
-   btop
-   cava
-   ranger
-   lazygit
-   stow
-   npm
-   nodejs
-   python3
-   pip
-   lua
-   luarocks
-   gloang
-   rust
-   fish
+  neovim
+  fastfetch
+  stow
+  btop
+  cava
+  lazygit
+  npm
+  nodejs
+  python3
+  pip
+  lua
+  luajit
+  luarocks
+  gloang
+  rust
+  fish
+
+  lua-language-server
+  stylua
 )
 
 ## npm packages(required by neovim plugins)
 npm_g_packages=(
-   neovim
-   yarn
-   pnpm
-   browser-sync
-   vscode-langservers-extracted
-   typescript
-   typescript-language-server
-   @vue/language-server
-   @vue/typescript-plugin
+  @vue/language-server@2.0.22
+  @vue/typescript-plugin@2.0.21
+  all-the-package-names@2.0.1583
+  browser-sync@3.0.2
+  eslint@9.5.0
+  neovim@5.1.0
+  pnpm@9.4.0
+  typescript-language-server@4.3.3
+  typescript@5.4.5
+  yarn@1.22.22
 )
 
 # set up path for global npm packages
 PREFIX="$HOME/.npm/packages"
 if [ ! -d "$PREFIX" ]; then
-   echo "Creating prefix directory: $PREFIX"
-   mkdir -p "$PREFIX"
+  echo "Creating prefix directory: $PREFIX"
+  mkdir -p "$PREFIX"
 fi
 npm config set prefix "$PREFIX"
 echo "Global npm prefix set to: $PREFIX"
 
 # install packages
-sudo dnf install "${packages[@]}"
+pkg install "${packages[@]}"
 npm i -g "${npm_g_packages[@]}"
-
-# seriously why mpd don't use these paths instead
-mkdir ~/.local/share/mpd/
-mkdir ~/.local/share/mpd/lyrics
-mkdir ~/.local/share/mpd/playlists
-touch ~/.local/share/mpd/pid
-touch ~/.local/share/mpd/database
 
 # bak up .bashrc just in case :)
 mv $HOME/.bashrc $HOME/.bashrc.bak
